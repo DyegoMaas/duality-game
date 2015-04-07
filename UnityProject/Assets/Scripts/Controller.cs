b1bc2648c7e3156f40623af2d4bfd933c1a20187
Dyego Maas <dyego.maas@gmail.com>
2015-04-07 20:23:45 -0300

    DM:2: criando versão inicial do jogo.

---
+++ UnityProject/Assets/Scripts/Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject leftSphere, rightSphere;
+    public float distanceBetweenSpheres = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ UnityProject/Assets/Scripts/Editor/ControllerEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Controller))]
+public class ControllerEditor : Editor
+{
+    public SerializedProperty distanceBetweenSpheres;
+
+    public void OnEnable()
+    {
+
+        distanceBetweenSpheres = serializedObject.FindProperty("distanceBetweenSpheres");
+    }
+
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        // Update the serializedProperty - always do this in the beginning of OnInspectorGUI.
+        serializedObject.Update();
+
+        Debug.Log("distanceBetweenSpheres is null" + (distanceBetweenSpheres == null));
+        //EditorGUILayout.PropertyField(distanceBetweenSpheres);
+
+        var displacement = distanceBetweenSpheres.floatValue / 2;
+        var controller = GameObject.Find(target.name).GetComponent<Controller>();
+        DisplaceBy(controller.leftSphere.transform, x: -displacement);
+        DisplaceBy(controller.rightSphere.transform, x: displacement);
+
+
+        // Apply changes to the serializedProperty - always do this in the end of OnInspectorGUI.
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void DisplaceBy(Transform transform, float x)
+    {
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
